b621d936fc4fc7624b75a467ab1a7cb62eeea94d
machop2016 <machobarbal@gmail.com>
2016-03-18 22:32:55 -0600

    Se quita los structs ya que los tiene el head

--- Alien.c
+++ Alien.c
@@ -1,29 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-struct alien_t{
-char nombre[30];
-int id;
-struct planeta_t *planetas;
-struct idioma_t *idiomas;
-struct alien_t *Siguiente;
-}alien;
-
-struct planeta_t{
-char nombre[30];
-int starwars_code;
-char agua;
-char oxigeno;
-struct planeta_t *Siguiente;
-}planeta;
-
-struct idioma_t{
-char nombre[30];
-char tipo;
-int simbolo;
-struct idioma_t *Siguiente;
-struct planeta_t *planeta;
-}idioma;
+#include <head.h>
 
 int main(){
 struct alien_t *lista_aliens;
